9cfb432d7af6df56dec5efc9c7b74e2d8618cd10
HonyTiana <honytiana@gmail.com>
2021-06-17 11:50:56 +0300

    [IMPL] edit company model

--- sofraco-back-end/src/models/company.js
+++ sofraco-back-end/src/models/company.js
@@ -3,13 +3,14 @@ const Schema = mongoose.Schema;
 
 const companySchema = new Schema({
     name: { type: String, required: true },
-    address: { type: String, default: true },
-    zip: { type: String, default: true },
-    city: { type: String, default: true },
-    country: { type: String, default: true },
-    creation_date: { type: Date, default: true },
-    phone: { type: String, default: true },
-    website: { type: String, default: true },
+    logo: { type: String, required: true },
+    address: { type: String, default: '' },
+    zip: { type: String, default: '' },
+    city: { type: String, default: '' },
+    country: { type: String, default: '' },
+    creation_date: { type: Date, default: '' },
+    phone: { type: String, default: '' },
+    website: { type: String, default: '' },
     is_enabled: { type: Boolean, default: true },
 });
 
