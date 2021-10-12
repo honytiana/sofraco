94f2f8bf29f9da6462940cb062ad88682a63a897
DanielSautot <72737048+DanielSautot@users.noreply.github.com>
2021-10-12 10:13:38 +0300

    Update seed.js

--- sofraco-db-seed/seed.js
+++ sofraco-db-seed/seed.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"undefined": "mongodb://127.20.0.2:27017/sofraco",
+	"undefined": "mongodb://172.20.0.2:27017/sofraco",
 	"dev": "localhost/DEV_DB_NAME",
 	"prod": "localhost/PROD_DB_NAME"
 }
