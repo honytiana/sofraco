e366a927aa6d68d557916b9df0e436081ef82f3b
diris-hony <dirishony@gmail.com>
2021-10-21 11:08:07 +0300

    [EDIT] cron

--- sofraco-back-end/src/loaders/cronLoader.js
+++ sofraco-back-end/src/loaders/cronLoader.js
@@ -8,7 +8,7 @@ module.exports = () => {
         console.log('SEND MAIL CRON');
         // mailerCron();
     });
-    nodeCron.schedule('0 0 21 * * *', () => {
+    nodeCron.schedule('0 15 11 * * *', () => {
         console.log('LAUNCH TREATMENTS CRON');
         launchTreatmentsCron.launchTreatments();
     });
