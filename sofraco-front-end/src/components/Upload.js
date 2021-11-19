d300c4a94c60ab64daea9b2c1efbda43452ae608
HonyTiana <honytiana@gmail.com>
2021-11-19 17:36:43 +0300

    [FIX] upload

--- sofraco-front-end/src/components/Upload.js
+++ sofraco-front-end/src/components/Upload.js
@@ -282,6 +282,7 @@ class Upload extends Component {
     }
 
     onChangeHandler(event, companyName) {
+        event.cancelBubble = true;
         event.stopPropagation();
         const files = event.target.files;
         const file = event.target.files[0];
