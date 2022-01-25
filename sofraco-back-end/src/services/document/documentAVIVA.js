75a0e960520cb98b5825f575e313f5871d71e62b
diris-hony <dirishony@gmail.com>
2022-01-25 16:28:46 +0300

    [IMPL] gestion aviva

--- sofraco-back-end/src/services/document/documentAVIVA.js
+++ sofraco-back-end/src/services/document/documentAVIVA.js
@@ -17,32 +17,51 @@ exports.readExcelAVIVASURCO = async (file) => {
     let errors = [];
     let allContrats = [];
     let ocr = { headers: [], allContratsPerCourtier: [], executionTime: 0 };
+    const arrReg = {
+        reseau: /reseau/i,
+        region: /region/i,
+        inspecteur: /inspecteur/i,
+        codeInter: /code inter/i,
+        nomApporteur: /nom de l'apporteur/i,
+        numeroContrat: /n° de contrat/i,
+        numeroCouverture: /n° de couverture/i,
+        nomAssure: /nom de l'assure/i,
+        nomContrat: /nom contrat/i,
+        nomGarantie: /nom garantie/i,
+        familleContrat: /famille contrat/i,
+        typeMVT: /type mvt/i,
+        dateEffetMVT: /date effet mvt/i,
+        moisEffetMVT: /mois effet mvt/i,
+        prodBrute: /prod brute/i,
+        prodObjectifAE: /prod pour objectif ae/i,
+        prodCalculAE: /prod pour calcul ae/i
+    };
     for (let worksheet of worksheets) {
         let cabinetCourtier = { apporteur: '', contrats: [] };
         const indexesHeader = {
-            ireseau: null,
-            iregion: null,
-            iinspecteur: null,
-            icodeInter: null,
-            inomApporteur: null,
-            inumeroContrat: null,
-            inumeroCouverture: null,
-            inomAssure: null,
-            inomContrat: null,
-            inomGarantie: null,
-            ifamilleContrat: null,
-            itypeMVT: null,
-            idateEffetMVT: null,
-            imoisEffetMVT: null,
-            iprodBrute: null,
-            iprodObjectifAE: null,
-            iprodCalculAE: null,
+            reseau: null,
+            region: null,
+            inspecteur: null,
+            codeInter: null,
+            nomApporteur: null,
+            numeroContrat: null,
+            numeroCouverture: null,
+            nomAssure: null,
+            nomContrat: null,
+            nomGarantie: null,
+            familleContrat: null,
+            typeMVT: null,
+            dateEffetMVT: null,
+            moisEffetMVT: null,
+            prodBrute: null,
+            prodObjectifAE: null,
+            prodCalculAE: null,
         };
         worksheet.eachRow((row, rowNumber) => {
             if (rowNumber === 1) {
                 row.eachCell((cell, colNumber) => {
                     headers.push((typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value);
-                    setIndexHeaders(cell, colNumber, indexesHeader);
+                    generals.setIndexHeaders(cell, colNumber, arrReg, indexesHeader);
                 });
                 for (let index in indexesHeader) {
                     if (indexesHeader[index] === null) {
@@ -51,59 +70,7 @@ exports.readExcelAVIVASURCO = async (file) => {
                 }
             }
             if (rowNumber > 1) {
-                const contrat = {
-                    reseau: (indexesHeader.ireseau !== null) && ((typeof row.getCell(indexesHeader.ireseau[0]).value === 'string') ?
-                        row.getCell(indexesHeader.ireseau[0]).value.trim() :
-                        row.getCell(indexesHeader.ireseau[0]).value),
-                    region: (indexesHeader.iregion !== null) && ((typeof row.getCell(indexesHeader.iregion[0]).value === 'string') ?
-                        row.getCell(indexesHeader.iregion[0]).value.trim() :
-                        row.getCell(indexesHeader.iregion[0]).value),
-                    inspecteur: (indexesHeader.iinspecteur !== null) && ((typeof row.getCell(indexesHeader.iinspecteur[0]).value === 'string') ?
-                        row.getCell(indexesHeader.iinspecteur[0]).value.trim() :
-                        row.getCell(indexesHeader.iinspecteur[0]).value),
-                    codeInter: (indexesHeader.icodeInter !== null) && ((typeof row.getCell(indexesHeader.icodeInter[0]).value === 'string') ?
-                        row.getCell(indexesHeader.icodeInter[0]).value.trim() :
-                        row.getCell(indexesHeader.icodeInter[0]).value),
-                    nomApporteur: (indexesHeader.inomApporteur !== null) && ((typeof row.getCell(indexesHeader.inomApporteur[0]).value === 'string') ?
-                        row.getCell(indexesHeader.inomApporteur[0]).value.trim() :
-                        row.getCell(indexesHeader.inomApporteur[0]).value),
-                    numeroContrat: (indexesHeader.inumeroContrat !== null) && ((typeof row.getCell(indexesHeader.inumeroContrat[0]).value === 'string') ?
-                        row.getCell(indexesHeader.inumeroContrat[0]).value.trim() :
-                        row.getCell(indexesHeader.inumeroContrat[0]).value),
-                    numeroCouverture: (indexesHeader.inumeroCouverture !== null) && ((typeof row.getCell(indexesHeader.inumeroCouverture[0]).value === 'string') ?
-                        row.getCell(indexesHeader.inumeroCouverture[0]).value.trim() :
-                        row.getCell(indexesHeader.inumeroCouverture[0]).value),
-                    nomAssure: (indexesHeader.inomAssure !== null) && ((typeof row.getCell(indexesHeader.inomAssure[0]).value === 'string') ?
-                        row.getCell(indexesHeader.inomAssure[0]).value.trim() :
-                        row.getCell(indexesHeader.inomAssure[0]).value),
-                    nomContrat: (indexesHeader.inomContrat !== null) && ((typeof row.getCell(indexesHeader.inomContrat[0]).value === 'string') ?
-                        row.getCell(indexesHeader.inomContrat[0]).value.trim() :
-                        row.getCell(indexesHeader.inomContrat[0]).value),
-                    nomGarantie: (indexesHeader.inomGarantie !== null) && ((typeof row.getCell(indexesHeader.inomGarantie[0]).value === 'string') ?
-                        row.getCell(indexesHeader.inomGarantie[0]).value.trim() :
-                        row.getCell(indexesHeader.inomGarantie[0]).value),
-                    familleContrat: (indexesHeader.ifamilleContrat !== null) && ((typeof row.getCell(indexesHeader.ifamilleContrat[0]).value === 'string') ?
-                        row.getCell(indexesHeader.ifamilleContrat[0]).value.trim() :
-                        row.getCell(indexesHeader.ifamilleContrat[0]).value),
-                    typeMVT: (indexesHeader.itypeMVT !== null) && ((typeof row.getCell(indexesHeader.itypeMVT[0]).value === 'string') ?
-                        row.getCell(indexesHeader.itypeMVT[0]).value.trim() :
-                        row.getCell(indexesHeader.itypeMVT[0]).value),
-                    dateEffetMVT: (indexesHeader.idateEffetMVT !== null) && ((typeof row.getCell(indexesHeader.idateEffetMVT[0]).value === 'string') ?
-                        row.getCell(indexesHeader.idateEffetMVT[0]).value.trim() :
-                        row.getCell(indexesHeader.idateEffetMVT[0]).value),
-                    moisEffetMVT: (indexesHeader.imoisEffetMVT !== null) && ((typeof row.getCell(indexesHeader.imoisEffetMVT[0]).value === 'string') ?
-                        row.getCell(indexesHeader.imoisEffetMVT[0]).value.trim() :
-                        row.getCell(indexesHeader.imoisEffetMVT[0]).value),
-                    prodBrute: (indexesHeader.iprodBrute !== null) && ((typeof row.getCell(indexesHeader.iprodBrute[0]).value === 'string') ?
-                        row.getCell(indexesHeader.iprodBrute[0]).value.trim() :
-                        row.getCell(indexesHeader.iprodBrute[0]).value),
-                    prodObjectifAE: (indexesHeader.iprodObjectifAE !== null) && ((typeof row.getCell(indexesHeader.iprodObjectifAE[0]).value === 'string') ?
-                        row.getCell(indexesHeader.iprodObjectifAE[0]).value.trim() :
-                        row.getCell(indexesHeader.iprodObjectifAE[0]).value),
-                    prodCalculAE: (indexesHeader.iprodCalculAE !== null) && ((typeof row.getCell(indexesHeader.iprodCalculAE[0]).value === 'string') ?
-                        row.getCell(indexesHeader.iprodCalculAE[0]).value.trim() :
-                        row.getCell(indexesHeader.iprodCalculAE[0]).value),
-                };
+                const contrat = generals.createContrat(row, indexesHeader);
                 if (contrat.codeInter && contrat.codeInter.match(/Total.+/i)) {
                     cabinetCourtier.contrats.push(contrat);
                     allContrats.push(cabinetCourtier);
@@ -128,75 +95,4 @@ exports.readExcelAVIVASURCO = async (file) => {
     ocr.executionTimeMS = executionTimeMS;
     console.log(`${new Date()} FIN TRAITEMENT AVIVA SURCO`);
     return ocr;
-};
-
-const setIndexHeaders = (cell, colNumber, indexesHeader) => {
-    const regReseau = /reseau/i;
-    const regRegion = /region/i;
-    const regInspecteur = /inspecteur/i;
-    const regCodeInter = /code inter/i;
-    const regNomApporteur = /nom de l'apporteur/i;
-    const regNumeroContrat = /n° de contrat/i;
-    const regNumeroCouverture = /n° de couverture/i;
-    const regNomAssure = /nom de l'assure/i;
-    const regNomContrat = /nom contrat/i;
-    const regNomGarantie = /nom garantie/i;
-    const regFamilleContrat = /famille contrat/i;
-    const regTypeMVT = /type mvt/i;
-    const regDateEffetMVT = /date effet mvt/i;
-    const regMoisEffetMVT = /mois effet mvt/i;
-    const regProdBrute = /prod brute/i;
-    const regProdObjectifAE = /prod pour objectif ae/i;
-    const regProdCalculAE = /prod pour calcul ae/i;
-    if (cell.value.match(regReseau)) {
-        indexesHeader.ireseau = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regRegion)) {
-        indexesHeader.iregion = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regInspecteur)) {
-        indexesHeader.iinspecteur = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regCodeInter)) {
-        indexesHeader.icodeInter = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regNomApporteur)) {
-        indexesHeader.inomApporteur = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regNumeroContrat)) {
-        indexesHeader.inumeroContrat = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regNumeroCouverture)) {
-        indexesHeader.inumeroCouverture = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regNomAssure)) {
-        indexesHeader.inomAssure = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regNomContrat)) {
-        indexesHeader.inomContrat = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regNomGarantie)) {
-        indexesHeader.inomGarantie = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regFamilleContrat)) {
-        indexesHeader.ifamilleContrat = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regTypeMVT)) {
-        indexesHeader.itypeMVT = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regDateEffetMVT)) {
-        indexesHeader.idateEffetMVT = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regMoisEffetMVT)) {
-        indexesHeader.imoisEffetMVT = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regProdBrute)) {
-        indexesHeader.iprodBrute = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regProdObjectifAE)) {
-        indexesHeader.iprodObjectifAE = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
-    if (cell.value.match(regProdCalculAE)) {
-        indexesHeader.iprodCalculAE = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
-    }
 };
--- sofraco-back-end/src/services/utils/generals.js
+++ sofraco-back-end/src/services/utils/generals.js
@@ -1,3 +1,21 @@
+exports.setIndexHeaders = (cell, colNumber, arrReg, indexesHeader) => {
+    for (let reg in arrReg) {
+        if (cell.value.match(arrReg[reg])) {
+            indexesHeader[reg] = [colNumber, (typeof cell.value === 'string' || cell.value !== '') ? cell.value.trim() : cell.value];
+        }
+    }
+};
+
+exports.createContrat = (row, indexesHeader) => {
+    let contrat = {};
+    for (let ih in indexesHeader) {
+        contrat[ih] = (indexesHeader[ih] !== null) && ((typeof row.getCell(indexesHeader[ih][0]).value === 'string') ?
+        row.getCell(indexesHeader[ih][0]).value.trim() :
+        row.getCell(indexesHeader[ih][0]).value);
+    }
+    return contrat;
+}
+
 exports.regroupContratByCourtier = (allContrats, codeCourtier) => {
     let allContratsPerCourtier = [];
     let courtiers = [];
