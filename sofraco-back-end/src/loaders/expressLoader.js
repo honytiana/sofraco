0e01c7c6a358cc078cf365fe7c2f6b7417e634cd
diris-hony <dirishony@gmail.com>
2022-04-12 20:21:06 +0300

    [IMPL] http proxy

--- sofraco-back-end/src/loaders/expressLoader.js
+++ sofraco-back-end/src/loaders/expressLoader.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const bcrypt = require('bcrypt');
+const httpProxy = require('http-proxy');
 const { logger } = require('../tools/logger');
 
 require('dotenv').config();
@@ -13,7 +14,11 @@ module.exports = async function ({ app }) {
         write: (text) => {
             logger.info(text);
         }
-    }
+    };
+    const proxy = httpProxy.createProxyServer({
+        target: `${process.env.NODE_SRC}:${process.env.PORT}`,
+        ws: true
+    });
     // app.use(body_parser.json());
     // app.use(body_parser.urlencoded());
     app.use(cookieParser());
@@ -31,6 +36,15 @@ module.exports = async function ({ app }) {
         res.setHeader('Access-Control-Allow-Credentials', 'true');
         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+        next();
+    });
+
+    app.use((req, res, next) => {
+        proxy.web(req, res, {
+            ws: true
+        }, (err) => {
+            console.log(err);
+        });
         next();
     });
 
